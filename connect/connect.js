d1f1cabf19a9a73c1e31832e7bcfa227140c17b5
Parth-Thacker <112555302+Parth-Thacker@users.noreply.github.com>
2023-01-23 10:36:45 +0530

    Update connect.js

--- connect/connect.js
+++ connect/connect.js
@@ -1,5 +1,6 @@
 const mongoose = require("mongoose")
-
+mongoose.set('strictQuery', true)
+mongoose.set('strictQuery', false);
 async function getConnection (){
     await mongoose.connect('mongodb+srv://Parth:parth1213@cluster0.ixp5oxm.mongodb.net/?retryWrites=true&w=majority')
 }
